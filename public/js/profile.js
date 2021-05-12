c32f80169226c6d821c7fd580b87282ec8a72aa5
Levi Kane <oneleggedlevi09@gmail.com>
2021-05-12 09:22:03 -0500

    blogs are now posting to the database

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,10 +2,9 @@ const newFormHandler = async (event) => {
   event.preventDefault();
 
   const name = document.querySelector('#blog-name').value.trim();
-  const needed_funding = document.querySelector('#blog-funding').value.trim();
   const description = document.querySelector('#blog-desc').value.trim();
 
-  if (name && needed_funding && description) {
+  if (name && description) {
     const response = await fetch(`/api/blogs`, {
       method: 'POST',
       body: JSON.stringify({ name, description }),
